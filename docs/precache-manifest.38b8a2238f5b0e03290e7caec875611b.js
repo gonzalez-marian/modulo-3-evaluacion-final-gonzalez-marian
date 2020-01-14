3109911e04fc8ed1562d4a4e690912d71e38af7b
Marian Gonzalez <gonzalezmarian170@gmail.com>
2020-01-14 16:53:46 +0100

    add docs

---
+++ docs/precache-manifest.38b8a2238f5b0e03290e7caec875611b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d172b95a10350aa706f5a771fc298c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc6ee19ddb9ea96d6845",
+    "url": "./static/css/main.00f36fd3.chunk.css"
+  },
+  {
+    "revision": "caa815dfe2cbf658658f",
+    "url": "./static/js/2.cdc746af.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.cdc746af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc6ee19ddb9ea96d6845",
+    "url": "./static/js/main.faf7eaea.chunk.js"
+  },
+  {
+    "revision": "ac1a0b4771bb62510ec9",
+    "url": "./static/js/runtime-main.7a540163.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_logo.75509f2e.png"
+  }
+]);
